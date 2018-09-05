e1276220d767a1ce735d2bb8cc02e8d956e17efd
victor de lange <victordelange2000@hotmail.com>
2018-09-05 12:59:43 +0200

    pizzacalculator
    calculator

---
+++ pizzacalculator.js
@@ -0,0 +1,18 @@
+var small = 7.50;
+var total = prompt("how many small pizz'a do you want?");
+
+document.write("Small pizza's =" + total);
+document.write("<br> Price = " + total * small);
+
+var medium = 9.50;
+var total = prompt("how many medium pizza's do you want?");
+
+document.write("<br><br> Medium pizza's =" + total);
+document.write("<br> Price = " + total * medium);
+
+var large = 12.00;
+var total = prompt("how many large pizza's do you want?");
+
+document.write("<br><br> Large pizza's = " + total);
+document.write("<br> Price = " + total * large);
+document.write("<br><br> total price ="+ total1+total2+total);
