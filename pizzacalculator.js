a700b239ceeec9544dde31d9352dd6b40d76d80b
victor de lange <victordelange2000@hotmail.com>
2018-09-11 09:56:14 +0200

    pizzacalculator met totaal

--- pizzacalculator.js
+++ pizzacalculator.js
@@ -1,18 +1,23 @@
 var small = 7.50;
-var total = prompt("how many small pizz'a do you want?");
-
-document.write("Small pizza's =" + total);
-document.write("<br> Price = " + total * small);
-
 var medium = 9.50;
-var total = prompt("how many medium pizza's do you want?");
-
-document.write("<br><br> Medium pizza's =" + total);
-document.write("<br> Price = " + total * medium);
-
 var large = 12.00;
-var total = prompt("how many large pizza's do you want?");
 
-document.write("<br><br> Large pizza's = " + total);
-document.write("<br> Price = " + total * large);
-document.write("<br><br> total price ="+ total1+total2+total);
+
+var totalsmall = prompt("how many small pizz'a do you want?");
+document.write("Small pizza's =" + totalsmall);
+var smallprice = totalsmall * small;
+
+
+var totalmedium = prompt("how many medium pizza's do you want?");
+document.write("<br><br> Medium pizza's =" + totalmedium);
+var mediumprice = totalmedium * medium;
+
+
+var totallarge = prompt("how many large pizza's do you want?");
+document.write("<br><br> Large pizza's = " + totallarge);
+var largeprice = totallarge * large;
+
+
+var total = smallprice + mediumprice + largeprice;
+
+document.write('<br><br>Total price : ' + total);
